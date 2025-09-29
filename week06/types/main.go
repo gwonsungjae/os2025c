5bc6a67f7e925cfbf1d0fefeeca3d8d38d840802
sung jae <gwonseongjae93@gmail.com>
2025-09-29 17:41:41 +0900

    v0.4.types/main.go

--- week06/types/main.go
+++ week06/types/main.go
@@ -11,9 +11,31 @@ func main() {
 	//fmt.Println(strings.Title("go developer!"))
 	//fmt.Println("Kim\nInha\t\"\\")
 	//fmt.Println('2', '가')
-	fmt.Println(reflect.TypeOf(2.31))
-	fmt.Println(reflect.TypeOf("sungjae"))
-	fmt.Println(reflect.TypeOf(true))
-	fmt.Println(reflect.TypeOf('A'))
-	fmt.Println(reflect.TypeOf(19))
+	//fmt.Println(reflect.TypeOf(2.31))
+	//fmt.Println(reflect.TypeOf("sungjae"))
+	//	fmt.Println(reflect.TypeOf(true))
+	//fmt.Println(reflect.TypeOf('A'))
+	//fmt.Println(reflect.TypeOf(19))
+
+	//var id int16
+	//var name string
+	//var gpa float32
+
+	//id = 999
+	//name = "sungjae"
+	//gpa = 3.99
+
+	//var id int16 = 999
+	//var name string = "sungjae"
+	//var gpa float32 = 3.99
+	//var id = 999
+	//var name = "sungjae"
+	//var gpa = 3.99
+
+	id := 999
+	name := "sungjae"
+	gpa := 3.99
+
+	fmt.Println("학번은", id, reflect.TypeOf(id), "이름은", name, reflect.TypeOf(name))
+	fmt.Println("평점 :", gpa, reflect.TypeOf(gpa))
 }
