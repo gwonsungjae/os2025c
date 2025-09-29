c2e01c33b1bd618bf290d3cca74fd4effca03673
sung jae <gwonseongjae93@gmail.com>
2025-09-29 18:13:31 +0900

    v0.6.types/main.go & ignore

--- week06/types/main.go
+++ week06/types/main.go
@@ -40,15 +40,20 @@ func main() {
 	//fmt.Println("평점 :", gpa, reflect.TypeOf(gpa))
 
 	//Zero values
-	var f64 float64
-	var t bool
-	var s string
-	var i int
-	var i16 int16
+	//var f64 float64
+	//var t bool
+	//var s string
+	//var i int
+	//var i16 int16
 
+	//mt.Println(f64, reflect.TypeOf(f64))
+	//fmt.Println(t, reflect.TypeOf(t))
+	//fmt.Println(s, reflect.TypeOf(s))
+	//fmt.Println(i, reflect.TypeOf(i))
+	//fmt.Println(i16, reflect.TypeOf(i16))
+
+	var f64 float64
+	totalPrice := 1000
+	fmt.Println((totalPrice))
 	fmt.Println(f64, reflect.TypeOf(f64))
-	fmt.Println(t, reflect.TypeOf(t))
-	fmt.Println(s, reflect.TypeOf(s))
-	fmt.Println(i, reflect.TypeOf(i))
-	fmt.Println(i16, reflect.TypeOf(i16))
 }
