f02c6d5878596bbe6bb997e3186227894d5a5fc3
sung jae <gwonseongjae93@gmail.com>
2025-10-13 18:06:18 +0900

    v0.3 bufio & os packages

--- week07/loops/main.go
+++ week07/loops/main.go
@@ -1,21 +1,48 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"os"
+	"strings"
+	"strconv"
+	"bufio"
 )
 
 func main() {
-
+	fmt.Print("Enter a score: ")
 	r := bufio.NewReader(os.Stdin)
-	//i, _ := r.ReadString('\n') /// ignore error
 	i, err := r.ReadString('\n')
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(i)
+
+	i = strings.TrimSpace(i)                // 문자열 주위에 붙은 공란 및 탭 키 등 제거
+	score, err := strconv.ParseFloat(i, 64) // 정리된 문자열을 실수타입으로 변환
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var status string
+	if score >= 60 {
+		status = "Pass"
+	} else {
+		status = "Fail"
+	}
+	fmt.Println(score, status)}
+
+
+	//var float64 float64 = 2.71
+ 	//var f float64 = 3.001
+	//fmt.Println(float64)
+
+
+	//var fmt float64 = 2.71
+	//r := bufio.NewReader(os.Stdin)
+	//i, _ := r.ReadString('\n') /// ignore error
+	//i, err := r.ReadString('\n')
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//fmt.Println(i)
 
 	//var now time.Time = time.Now()
 
