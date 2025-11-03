e8e576ff7627b9448a2e1c388b08dca46888447c
sung jae <gwonseongjae93@gmail.com>
2025-11-03 18:23:17 +0900

    week10.array v0.5

--- week10/cmd/ch05-ex01/main.go
+++ week10/cmd/ch05-ex01/main.go
@@ -6,20 +6,24 @@ import (
 
 func main() {
 	//var arrayBool [3]bool = [3]bool{true, false, true}
-	arrayBool := [2]bool{true, false}
+	//arrayBool := [2]bool{true, false}
 	//var arrayInt [3]int
+	// arrayInt := [3]int{-9, 11, 7}
+	// for i := 0; i < len(arrayInt); i++ {
+	// 	//fmt.Println(i, arrayBool[i])
+	// 	fmt.Println(i, arrayInt[i])
+	// }
 	arrayInt := [3]int{-9, 11, 7}
-	for i := 0; i < len(arrayBool); i++ {
-		fmt.Println(i, arrayBool[i])
-		fmt.Println(i, arrayInt[i])
+	for i, arrayInt := range arrayInt {
+		fmt.Println(i, arrayInt)
 	}
 
 	//fmt.Println(reflect.TypeOf(arrayBool))
 	//fmt.Println("%#v\n", arrayBool)
 
-	fmt.Println(arrayBool[1])
-	arrayInt[1] = 2
-	fmt.Println(arrayInt[1])
+	//fmt.Println(arrayBool[1])
+	//arrayInt[1] = 2
+	//fmt.Println(arrayInt[1])
 
 	//fmt.Println((arrayBool[1]))
 	//arrayInt[1]++
