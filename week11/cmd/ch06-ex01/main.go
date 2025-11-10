9825aa0d5ab54616925d5cb297a4ec66057391d2
sung jae <gwonseongjae93@gmail.com>
2025-11-10 17:14:06 +0900

    week11 slice v0.1

---
+++ week11/cmd/ch06-ex01/main.go
@@ -0,0 +1,15 @@
+// Package datafile allows reading data samples from files.
+package main
+
+import "fmt"
+
+func main() {
+	var sub []string
+	sub = make([]string, 3)
+	sub[0] = "Go"
+	sub[2] = "Python"
+
+	for _, subject := range sub {
+		fmt.Println(subject)
+	}
+}
