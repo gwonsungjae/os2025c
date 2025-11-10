46eabfff7967c62a4b6aa63b790dabb729073bd7
sung jae <gwonseongjae93@gmail.com>
2025-11-10 17:39:21 +0900

    week11 slice v0.5

--- week11/cmd/ch06-ex01/main.go
+++ week11/cmd/ch06-ex01/main.go
@@ -7,17 +7,20 @@ func main() {
 	//var sub []string
 	//sub = make([]string, 3)
 	//sub := make([]string, 3)
-	sub := []string{"Go", "Javascript", "Python", "Linux"}
+	sub := [4]string{"Go", "Javascript", "Python", "Linux"}
 	//sub[0] = "Go"
 	//sub[2] = "Python"
 
-	subSlice := sub[1:3]
+	subSlice := sub[:3]
+	//sub[0] = "Java"
+	subSlice[0] = "Database"
+	for _, subject := range sub {
+		fmt.Println(subject)
+	}
+
 	fmt.Println("===============")
 	for i := 0; i < len(subSlice); i++ {
 		fmt.Println(subSlice[i])
 	}
 
-	for _, subject := range sub {
-		fmt.Println(subject)
-	}
 }
