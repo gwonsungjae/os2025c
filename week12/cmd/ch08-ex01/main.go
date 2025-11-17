639a2a26bf4c24c0c4f9f477f62988216a2890b2
sung jae <gwonseongjae93@gmail.com>
2025-11-17 18:13:42 +0900

    week12 struct v0.2

--- week12/cmd/ch08-ex01/main.go
+++ week12/cmd/ch08-ex01/main.go
@@ -11,9 +11,14 @@ func applyPrice(s *subscriber) {
 	s.price = 10000
 	s.name = "Kim Inha"
 }
+
 func main() {
 	var s1 subscriber
-	//s1.name = "Lee Inha"
+	var p *subscriber = &s1
+	// s1.name = "Lee Inha"
 	applyPrice(&s1)
 	fmt.Println(s1.name, s1.price)
+	// fmt.Println(*p.price)
+	// fmt.Println((*p).price)
+	fmt.Println(p.price)
 }
